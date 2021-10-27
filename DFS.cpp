208637fda1c990fc881af670ba93155ca574c180
Shounak <shounakghosh01@gmail.com>
2021-10-27 21:31:25 +0530

    Update DFS.cpp

--- DFS.cpp
+++ DFS.cpp
@@ -1,2 +1,39 @@
-#include <bits/stdc++.h>
+#include<bits/stdc++.h>
 using namespace std;
+#define pb push_back
+const int N=1e5+5;
+vector<bool> visited(N,false);
+vector<int> adjl[N];
+
+void dfs(int node)
+{
+    //preorder
+    visited[node]=true;
+    cout<<node<<" ";
+
+    //inorder
+    for(auto x:adjl[node])
+    {
+        if(!visited[x])
+            dfs(x);
+    }
+    
+}
+
+int main()
+{
+    int n,m,root;
+    cin>>n>>m;
+    for (int i=0;i<m;i++)
+    {
+        int x,y;
+        cin>>x>>y;
+        adjl[x].pb(y);
+        adjl[y].pb(x);
+    }
+    cin>>root;
+
+    dfs(root);
+    return 1;
+
+}
